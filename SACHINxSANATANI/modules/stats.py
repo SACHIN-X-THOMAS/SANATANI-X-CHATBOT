2818add02a6faf43b7325f713c6f7efa8e6c22ee
SACHIN SANATANI <159048376+SACHINxSANATANI@users.noreply.github.com>
2024-03-27 11:21:03 +0530

    Update stats.py

--- SACHINxSANATANI/modules/stats.py
+++ SACHINxSANATANI/modules/stats.py
@@ -6,7 +6,7 @@ from SACHINxSANATANI.database.chats import get_served_chats
 from SACHINxSANATANI.database.users import get_served_users
 
 
-@dev.on_message(filters.command("stats") & filters.user(OWNER))
+@dev.on_message(filters.command("ISstats") & filters.user(OWNER))
 async def stats(cli: dev, message: Message):
     users = len(await get_served_users())
     chats = len(await get_served_chats())
